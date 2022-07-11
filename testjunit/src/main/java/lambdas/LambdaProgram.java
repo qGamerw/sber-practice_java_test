474ab34595cc60ae3f5e1c899fa2f9bc9c0c69b3
Misha Uxin <95779386+qGamerw@users.noreply.github.com>
2022-07-11 23:49:06 +0300

    Update LambdaProgram.java

--- testjunit/src/main/java/lambdas/LambdaProgram.java
+++ testjunit/src/main/java/lambdas/LambdaProgram.java
@@ -21,31 +21,4 @@ public class LambdaProgram {
     private static void extracted(NullParameterFunction param) {
         param.doNullSomething();
     }
-
-    public static void main(String[] args) {
-
-        extracted(new NullParameterFunction() {
-            @Override
-            public void doNullSomething() {
-                System.out.println("Called via an anonymous class");
-            }
-
-        });
-
-        extracted(() -> System.out.println("Called via Lambda"));
-
-        //4
-        Function<Integer, Double> half = a -> a / 2.0;
-        System.out.println(half.apply(10));
-
-        Predicate<Integer> predicate = a -> (a > 18);
-        System.out.println("a > 18: " + predicate.test(10));
-
-        String srt = "Hello";
-        Supplier<String> supplierStr = () -> srt.toUpperCase();
-        System.out.println(supplierStr.get());
-
-        Consumer<String> print = x -> System.out.println(x);
-        print.accept("Hello");
-    }
 }
