0698bec76b9809ff4808f81383f14f591cba0703
qGamerw <mish.uxin2012@yandex.ru>
2022-07-14 11:32:50 +0300

    fix bag

--- testjunit/src/main/java/multithreading/Resource.java
+++ testjunit/src/main/java/multithreading/Resource.java
@@ -1,22 +1,41 @@
 package multithreading;
 
 public class Resource {
-    private volatile int number = 0;
+    private int number = 0;
 
-    public synchronized void addResource() throws InterruptedException {
-        if (number > 5)
-            wait();
-        number++;
-        notify();
-    }
-
-    public synchronized void removeResource() throws InterruptedException {
-        if (number < 1)
-            wait();
-        number--;
-        notify();
-    }
     public static Runnable convertToRunnable(Runnable runnable) {
         return runnable;
+    }
+
+    public synchronized void addResource() {
+        for (int i = 0; i < 10; i++) {
+            while (number > 5) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            number++;
+            System.out.println(number);
+            notify();
+        }
+
+    }
+
+    public synchronized void removeResource() {
+        for (int i = 0; i < 10; i++) {
+            while (number < 1) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            number--;
+            System.out.println(number);
+            notify();
+        }
+
     }
 }
